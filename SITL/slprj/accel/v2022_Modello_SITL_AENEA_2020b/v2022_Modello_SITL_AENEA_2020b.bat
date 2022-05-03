137793292634c63c39b325b95bbb573d36e4cb78
eEmanuelePerfetti <perfettiemanuele5@gmail.com>
2022-05-03 14:49:38 +0200

    commit risolutivo

--- SITL/slprj/accel/v2022_Modello_SITL_AENEA_2020b/v2022_Modello_SITL_AENEA_2020b.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set MATLAB=C:\Program Files\MATLAB\R2020b
-"%MATLAB%\bin\win64\gmake" -f v2022_Modello_SITL_AENEA_2020b.mk  OPTS="-DTID01EQ=1"
--- SITL/slprj/sim/_sharedutils/xgetrfs_6diCZfCG.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTW_HEADER_xgetrfs_6diCZfCG_h_
-#define RTW_HEADER_xgetrfs_6diCZfCG_h_
-#include "rtwtypes.h"
-#include "multiword_types.h"
-
-extern int32_T xgetrfs_6diCZfCG(real_T A[121], real_T B[198]);
-
-#endif

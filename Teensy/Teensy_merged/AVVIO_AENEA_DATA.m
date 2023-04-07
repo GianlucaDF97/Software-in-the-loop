d558e6674b316710f576e4738c3724428cd6f0dc
Davide De Santis <davide312000@gmail.com>
2023-04-07 12:45:04 +0200

    teensy_merged
    teensy_merged contiene teensy_completo con l'aggiunta del log della pressione differenziale

---
+++ Teensy/Teensy_merged/AVVIO_AENEA_DATA.m
@@ -0,0 +1,127 @@
+filepath = fileparts(mfilename('fullpath'));
+% addpath(filepath,...
+%         [filepath filesep 'teensy_ec'],...
+%         [filepath filesep 'src'],...
+%         [filepath filesep 'sfunctions'],...
+%         [filepath filesep 'Utility']);
+    
+basePath = filepath;    
+
+busdef();
+% addpath(genpath('C:\Program Files (x86)\Arduino\hardware\teensy\avr'));
+
+%  FILE PER CARICAMENTO VARIABILI SU AUTOPILOTA %
+
+t_AP = 0.02;
+
+%% SEZIONE CARATTERISTICHE VELIVOLO
+
+V_stall = 11;
+V_max = 30;
+
+%% SEZIONE ROTAZIONE SUPERFICI MOBILI
+
+attuatore_Amax = deg2rad(27);
+attuatore_Amin = deg2rad(-23);
+attuatore_Emax = deg2rad(18);
+attuatore_Emin = deg2rad(-32);
+attuatore_Rmax = deg2rad(30);
+attuatore_Rmin = deg2rad(-30);
+attuatore_Tmax = 1;
+attuatore_Tmin = 0;
+
+theta_max = deg2rad(15);
+theta_min = deg2rad(-15);
+phi_max = deg2rad(30);
+phi_min = deg2rad(-30);
+%% SEGNALI PWM MAX E MIN
+pwm_throttle_max=1;
+pwm_throttle_min=1;
+pwm_elevator_max=1;
+pwm_elevator_min=1;
+pwm_aileron_max=1;
+pwm_aileron_min=1;
+pwm_rudder_max=1;
+pwm_rudder_min=1;
+%% INVERSIONE COMANDI
+inv_T = 1;            % inverti motore
+inv_E = 1;            % inverti equilibratore
+inv_A = -1;            % inverti alettoni
+inv_R = 1;            % inverti timone
+inv_comp_h = -1;        % inverti compensazione quota
+%% SEZIONE SAFE
+
+interventoA_05 = 6;
+interventoA_1 = 2;
+interventoE_05 = 7.5;
+interventoE_1 = 3.2;
+interventoT = 0.1;
+safe_phi = 55;
+safe_V = 40;
+time_alarm_SAFE = 1;
+
+%% SEZIONE TUNING GUADAGNI DEI CONTROLLORI 
+
+% IN - PARAMETRI SISTEMA DI CONTROLLO -
+Kp_becch = -0.1918*10;                      % Kp controllore beccheggio
+Ki_becch = -0.064065*10;                    % Ki controllore beccheggio
+Kp_q = -0.025*5;                           % Kp controllore velocita beccheggio
+Kp_comp_h = 0.035*15;                       % Kp compensatore di quota
+
+Kp_roll = 0.11*10;                          % Kp controllore rollio
+Ki_roll = 0.025*10;                         % Ki controllore rollio
+Kp_p = 0.025*5;                            % Kp controllore velocita rollio
+
+Kp_imb = -0.04*10;                         % Kp controllore imbardata
+
+Kp_AP_vel = 0.2692/8;                      % Kp controllore velocita
+Ki_AP_vel = 0.001*8;                       % Kp controllore velocita
+
+Kp_AP_quota_hold_h = 0.15;                  % Kp controllore quota ad alte velocita
+Kp_AP_quota_hold_l = 1;                     % Kp controllore quota a basse velocita
+Kp_AP_quota_select = 0.014;                 % Kp controllore di quota
+Ki_AP_quota_select = 0.014;                 % Ki controllore di quota
+
+Kp_AP_rotta = 0.7;  %0.5                    % Kp controllore di rotta
+Ki_AP_rotta = 0.03;                         % Ki controllore di rotta
+Kd_AP_rotta = 0.9; %0.35                    % Kd controllore di rotta
+
+K_imb_inv = -1;                             % Kp imbardata inversa
+
+K_AW_B_becc = -250;                         % Guadagno bumpless beccheggio
+K_AW_B_roll =  500;                         % Guadagno bumpless rollio
+K_AW_B_imb = 0;                             % Guadagno bumpless imbardata
+K_AW_B_AP_quota = 1000;                     % Guadagno bumpless quota
+
+% IN - GESTIONE CONTROLLORI DI QUOTA
+Kp_h_cruise = 0.04;                         % Kp controllore quota in crociera
+Ki_h_cruise = 0.008;                        % Ki controllore quota in crociera   
+
+Kp_h_app_max = 0.086;                       % Kp controllore quota in fase di approach
+Kp_h_app_min = 0.01;                        % Kp controllore quota in fase di approach
+Ki_h_app = 0.055;                           % Ki controllore quota in fase di approach
+Kd_h_app = 0.085;                           % Kd controllore quota in fase di approach
+
+Kp_h_flare = 0.1;                          % Kp controllore quota in fase di flare
+Ki_h_flare = 0.07;                          % Ki controllore quota in fase di flare
+Kd_h_flare = 0.01; 
+
+
+
+% IN - CONTROLLORE DI ROTTA -
+Kp_Rotta_AL = 5;
+Kp_Rotta_ATO = 4;
+% IN - CONTROLLORE DI IMBARDATA -
+Kp_Rotta_psi = -0.01;
+
+%% PARAMETRI DECOLLO AUTOMATICO
+
+final_h = 35;     %quota finale raggiunta
+theta_rot = 10;   % angolo di beccheggio in fase di rotazione
+
+
+
+%% SEZIONE WAYPOINT
+
+vel_wp1_al = 25;
+vel_wp2_al = 20;
---
+++ Teensy/Teensy_merged/Aenea_Teensy_code/Aenea_Teensy_code/Aenea_Teensy_code/Aenea_Teensy_code/Aenea_model_data.cpp
@@ -0,0 +1,99 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: Aenea_model_data.cpp
+//
+// Code generated for Simulink model 'Aenea_model'.
+//
+// Model version                  : 1.152
+// Simulink Coder version         : 9.4 (R2020b) 29-Jul-2020
+// C/C++ source code generated on : Mon Mar 13 15:23:14 2023
+//
+// Target selection: teensy_ec.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex-M
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include "Aenea_model.h"
+#include "Aenea_model_private.h"
+
+// Invariant block signals (default storage)
+const ConstB_Aenea_model_T Aenea_model_ConstB = {
+  0.011111111111111112
+  ,                                    // '<S22>/USCITA PERCENTUALE'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion5'
+  0.0
+  ,                                    // '<S9>/Switch'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion13'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion16'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion14'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion17'
+  1.0
+  ,                                    // '<S9>/Data Type Conversion15'
+  1400.0
+  ,                                    // '<S593>/Sum2'
+  1400.0
+  ,                                    // '<S596>/Sum2'
+  1400.0
+  ,                                    // '<S594>/Sum2'
+  1400.0
+  ,                                    // '<S595>/Sum1'
+  0U
+  ,                                    // '<S16>/Data Type Conversion'
+  0U
+  ,                                    // '<S21>/Gain'
+  0U
+  ,                                    // '<S22>/Data Type Conversion2'
+  128
+  ,                                    // '<S8>/Gain3'
+  0
+  ,                                    // '<S21>/Gain2'
+  0
+  ,                                    // '<S21>/Gain1'
+  0
+  // '<S21>/Data Type Conversion3'
+};
+
+// Constant parameters (default storage)
+const ConstP_Aenea_model_T Aenea_model_ConstP = {
+  // Expression: R1B
+  //  Referenced by: '<S6>/Constant13'
+
+  { 2.0E-14, 2.0E-14, 0.1444, 0.2, 0.2, 0.0001486, 9.403E-5, 0.0001648,
+    0.0002934, 2.887E-5, 0.003258 },
+
+  // Expression: Q1B
+  //  Referenced by: '<S6>/Constant16'
+
+  { 2.0E-15, 2.0E-15, 0.001, 0.04, 0.04, 5.0E-5, 0.00019943, 7.324E-5, 0.0001065,
+    2.6647799999999998E-6, 2.6647799999999998E-6, 3.55304E-5, 6.0E-20, 6.0E-20,
+    6.0E-20, 6.0E-15, 6.0E-15, 6.0E-15 },
+
+  // Computed Parameter: onboard_control_sensor_presentu
+  //  Referenced by: '<S21>/onboard_control_sensor_present uint32'
+
+  { 1U, 1U, 1U },
+
+  // Computed Parameter: SIUAVuint8_Value
+  //  Referenced by: '<S1>/SI UAV uint8'
+
+  100U,
+
+  // Computed Parameter: registromemoria213_Value
+  //  Referenced by: '<S7>/registro memoria2 13'
+
+  { 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U, 1U }
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
